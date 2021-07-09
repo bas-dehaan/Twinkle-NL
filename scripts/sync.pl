fd0fd67db25e488dcd6a9da85f78848651447089
Bas de Haan <9017270+bas-dehaan@users.noreply.github.com>
2021-07-09 15:46:54 +0200

    update sync.pl

--- scripts/sync.pl
+++ scripts/sync.pl
@@ -468,14 +468,11 @@ twinkle.js
   modules/twinkledeprod.js
   modules/twinklediff.js
   modules/twinklefluff.js
-  modules/twinkleimage.js
-  modules/twinkleprod.js
   modules/twinkleprotect.js
   modules/twinklespeedy.js
   modules/twinkleunlink.js
   modules/twinklewarn.js
   modules/twinklexfd.js
-  modules/friendlyshared.js
   modules/friendlytag.js
   modules/friendlytalkback.js
   modules/friendlywelcome.js
